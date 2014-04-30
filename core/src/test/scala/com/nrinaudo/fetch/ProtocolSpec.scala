a811109ff6ab736439ef58131e47677641a4dd07
nrinaudo <nicolas@nrinaudo.com>
2014-04-30 22:43:50 +0200

    Added simple tests for Protocol.

--- core/src/test/scala/com/nrinaudo/fetch/ProtocolSpec.scala
+++ core/src/test/scala/com/nrinaudo/fetch/ProtocolSpec.scala
@@ -1,10 +1,22 @@
 package com.nrinaudo.fetch
 
 import org.scalacheck.Gen
+import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 object ProtocolSpec {
   /** Generates a random supported protocol (http or https). */
   def protocol = Gen.oneOf(Protocol.Http, Protocol.Https)
 }
 
-// TODO: add serialization / deserialization tests
+class ProtocolSpec extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
+  import ProtocolSpec._
+
+  describe("Protocol") {
+    it("should parse valid instances correctly") {
+      forAll(protocol) { protocol =>
+        Protocol.unapply(protocol.name) should be(Some(protocol))
+      }
+    }
+  }
+}
