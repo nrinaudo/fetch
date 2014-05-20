ecbd961d0d6dfc2793719a39fd1104f1ddb04ff7
nrinaudo <nicolas@nrinaudo.com>
2014-05-20 21:58:53 +0200

    Method is now case-insensitive, but stored as upper-case as is the norm.

--- core/src/main/scala/com/nrinaudo/fetch/Method.scala
+++ core/src/main/scala/com/nrinaudo/fetch/Method.scala
@@ -1,29 +1,31 @@
 package com.nrinaudo.fetch
 
 object Method {
-  object GET extends Method("GET")
-  object POST extends Method("POST")
-  object PUT extends Method("PUT")
-  object DELETE extends Method("DELETE")
-  object HEAD extends Method("HEAD")
-  object OPTIONS extends Method("OPTIONS")
-  object TRACE extends Method("TRACE")
-  object CONNECT extends Method("CONNECT")
-  object PATCH extends Method("PATCH")
-  object LINK extends Method("LINK")
-  object UNLINK extends Method("UNLINK")
+  val GET: Method     = MethodImpl("GET")
+  val POST: Method    = MethodImpl("POST")
+  val PUT: Method     = MethodImpl("PUT")
+  val DELETE: Method  = MethodImpl("DELETE")
+  val HEAD: Method    = MethodImpl("HEAD")
+  val OPTIONS: Method = MethodImpl("OPTIONS")
+  val TRACE: Method   = MethodImpl("TRACE")
+  val CONNECT: Method = MethodImpl("CONNECT")
+  val PATCH: Method   = MethodImpl("PATCH")
+  val LINK: Method    = MethodImpl("LINK")
+  val UNLINK: Method  = MethodImpl("UNLINK")
+
+  private case class MethodImpl(name: String) extends Method
 
   private val MethodPattern = """(\p{Alpha}+)""".r
 
+  def apply(value: String): Method = unapply(value) getOrElse {throw new IllegalArgumentException("Not a valid method: " + value)}
+
   def unapply(value: String): Option[Method] = value match {
-    case MethodPattern(method) => Some(Method(method))
-    case _                     => None
+    case MethodPattern(m) => Some(MethodImpl(m))
+    case _                => None
   }
 }
 
-/**
- * Represents a valid HTTP method.
- *
- * See the [[Method$ companion object]] for standard values.
- */
-case class Method(name: String)
+sealed trait Method {
+  val name: String
+  override def toString = name
+}
--- core/src/test/scala/com/nrinaudo/fetch/MethodSpec.scala
+++ core/src/test/scala/com/nrinaudo/fetch/MethodSpec.scala
@@ -1,6 +1,6 @@
 package com.nrinaudo.fetch
 
-import org.scalacheck.Gen
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.{Matchers, FunSpec}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
@@ -9,6 +9,8 @@ object MethodSpec {
   def httpMethod = Gen.oneOf(Method.GET, Method.POST, Method.PUT, Method.DELETE, Method.OPTIONS, Method.TRACE,
     Method.PATCH, Method.LINK, Method.UNLINK)
 
+  def invalidMethod: Gen[String] = Arbitrary.arbitrary[String].suchThat(_.exists(!_.isLetter))
+
   def httpMethods = for {
     count <- Gen.choose(1, 5)
     set   <- Gen.containerOfN[Set, Method](count, httpMethod)
@@ -16,31 +18,33 @@ object MethodSpec {
 }
 
 class MethodSpec extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
-  describe("Method") {
-    it("should unapply against known HTTP methods") {
-      Method.unapply("GET") should be(Some(Method.GET))
-      Method.unapply("POST") should be(Some(Method.POST))
-      Method.unapply("PUT") should be(Some(Method.PUT))
-      Method.unapply("DELETE") should be(Some(Method.DELETE))
-      Method.unapply("HEAD") should be(Some(Method.HEAD))
-      Method.unapply("OPTIONS") should be(Some(Method.OPTIONS))
-      Method.unapply("TRACE") should be(Some(Method.TRACE))
-      Method.unapply("CONNECT") should be(Some(Method.CONNECT))
-      Method.unapply("PATCH") should be(Some(Method.PATCH))
-      Method.unapply("LINK") should be(Some(Method.LINK))
-      Method.unapply("UNLINK") should be(Some(Method.UNLINK))
+  import MethodSpec._
+
+  describe("The Method companion object") {
+    it("should unapply on known methods") {
+      forAll(httpMethod) { method => Method.unapply(method.name) should be(Some(method)) }
     }
 
-    it("should unapply against unknown but legal HTTP methods") {
-      forAll(Gen.alphaStr.suchThat(!_.isEmpty)) { method =>
-        Method.unapply(method) should be(Some(Method(method)))
-      }
+    it("should apply on known mehods") {
+      forAll(httpMethod) { method => Method(method.name) should be(method) }
     }
 
-    it("should not unapply against illegal HTTP methods") {
-      forAll(Gen.identifier, Gen.identifier) { (a, b) =>
-        Method.unapply(a + ' ' + b) should be(None)
-      }
+    it("should unapply on unknown but legal HTTP methods") {
+      forAll(Gen.alphaStr.suchThat(!_.isEmpty)) { method => Method.unapply(method) should be(Some(Method(method))) }
+    }
+
+    it("should not unapply on illegal HTTP methods") {
+      forAll(invalidMethod) { method => Method.unapply(method) should be(None) }
+    }
+
+    it("should fail to apply on illegal HTTP methods") {
+      forAll(invalidMethod) { method => intercept[IllegalArgumentException](Method(method)) }
+    }
+  }
+
+  describe("A Method instance") {
+    it("should serialize to itself") {
+      forAll(httpMethod) { method => Method(method.toString) should be(method) }
     }
   }
 }
