17e6e8fe19fff440a7df73a55a292924746d1782
nrinaudo <nicolas@nrinaudo.com>
2014-05-23 12:23:57 +0200

    Fixed an issue where connections failed to be kept alive because of GZipInputStream and DeflaterInputStream not reading all the way to -1. Pure supputations, but it fits the symptoms and fixes the issue.

--- core/src/main/scala/com/nrinaudo/fetch/ResponseEntity.scala
+++ core/src/main/scala/com/nrinaudo/fetch/ResponseEntity.scala
@@ -1,7 +1,7 @@
 package com.nrinaudo.fetch
 
 import java.nio.charset.Charset
-import java.io.{Reader, InputStreamReader, InputStream}
+import java.io.{FilterInputStream, Reader, InputStreamReader, InputStream}
 import com.nrinaudo.fetch.ResponseEntity.EntityParser
 
 object ResponseEntity {
@@ -13,12 +13,23 @@ object ResponseEntity {
  * @param mime   MIME type of the entity.
  * @param stream stream from which to read the content of the entity.
  */
-class ResponseEntity(val mime: Option[MimeType], private val stream: InputStream) {
+class ResponseEntity(val mime: Option[MimeType], stream: InputStream) {
   require(stream != null, "Response stream should never be null")
 
-  def decode(encoding: Encoding): ResponseEntity = new ResponseEntity(mime, encoding.decode(stream))
+  // It appears that some stream implementation (decompressing ones, for instance, such as GZipInputStream) stop reading
+  // before read returns -1 - which seems to prevent keeping connections alive.
+  // The following is a nasty workaround, but it works.
+  private val content = new FilterInputStream(stream) {
+    override def close(): Unit = {
+      in.read()
+      super.close()
+    }
+  }
 
-  def as[T : EntityParser]: T = implicitly[EntityParser[T]].apply(this)
+
+  def decode(encoding: Encoding): ResponseEntity = new ResponseEntity(mime, encoding.decode(content))
+
+  def as[T: EntityParser]: T = implicitly[EntityParser[T]].apply(this)
 
   /** Charset in which the entity is written, if any. */
   def charset: Option[Charset] = for {
@@ -31,15 +42,15 @@ class ResponseEntity(val mime: Option[MimeType], private val stream: InputStream
     * This method will take care of closing the underlying stream.
     */
   def withStream[T](f: InputStream => T) =
-    try {f(stream)}
-    finally {stream.close()}
+    try {f(content)}
+    finally {content.close()}
 
   /** Executes the specified function on this response entity.
     *
     * This method will take care of closing the underlying stream.
     */
   def withReader[T](f: Reader => T) = {
-    val reader = new InputStreamReader(stream, charset getOrElse DefaultCharset)
+    val reader = new InputStreamReader(content, charset getOrElse DefaultCharset)
     try {f(reader)}
     finally {reader.close()}
   }
