9730888cf299996ff88b4d4c2f8033ddabf72a8a
nrinaudo <nicolas@nrinaudo.com>
2014-05-30 18:33:19 +0200

    Further tests for HttpGrammar

--- core/src/main/scala/com/nrinaudo/fetch/HttpGrammar.scala
+++ core/src/main/scala/com/nrinaudo/fetch/HttpGrammar.scala
@@ -4,8 +4,8 @@ import scala.util.parsing.combinator._
 
 object HttpGrammar extends RegexParsers {
   override def skipWhitespace = false
-  def token: Parser[String] = """[\u0020-\u007E&&[^ \t()<>@,;:\"/\[\]?={}]]+""".r
-  def quotedPair: Parser[String] = "\\" ~> """[\u0000-\u007F]""".r
-  def qdtext: Parser[String] = """[\u0000-\u007f&&[^\"\\]]+""".r
+  def token: Parser[String]        = """[\u0020-\u007E&&[^ \t()<>@,;:\"/\[\]?={}]]+""".r
+  def quotedPair: Parser[String]   = "\\" ~> """[\u0000-\u007F]""".r
+  def qdtext: Parser[String]       = """[\u0000-\u007f&&[^\"\\]]+""".r
   def quotedString: Parser[String] = "\"" ~> (rep(quotedPair | qdtext) ^^ (_.mkString)) <~ "\""
 }
--- core/src/test/scala/com/nrinaudo/fetch/HttpGrammarSpec.scala
+++ core/src/test/scala/com/nrinaudo/fetch/HttpGrammarSpec.scala
@@ -8,10 +8,16 @@ object HttpGrammarSpec {
   val Separators = Set('(', ')', '<', '>', '@', ',', ';', ':', '\\', '"', '/', '[', ']', '?', '=', '{', '}', ' ', '\t')
 
   private val tokenChars = Gen.oneOf(((32.toChar to 126.toChar).toSet &~ Separators).toSeq)
+  private val qdtextChars = Gen.oneOf(((0.toChar to 127.toChar).toSet - '\"' - '\\').toSeq)
 
-  def token: Gen[String] = for {
-    chars <- Gen.nonEmptyListOf(tokenChars)
+  def char: Gen[Char] = Gen.oneOf(0.toChar, 127.toChar)
+
+  private def string(gen: Gen[Char]): Gen[String] = for {
+    chars <- Gen.nonEmptyListOf(gen)
   } yield chars.mkString
+
+  def token: Gen[String] = string(tokenChars)
+  def qdtext: Gen[String] = string(qdtextChars)
 }
 
 class HttpGrammarSpec extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
@@ -20,6 +26,18 @@ class HttpGrammarSpec extends FunSpec with Matchers with GeneratorDrivenProperty
   describe("HttpGrammar") {
     it("should parse valid tokens") {
       forAll(token) { token => HttpGrammar.parseAll(HttpGrammar.token, token).get should be(token) }
+    }
+
+    it("should parse valid quoted pairs") {
+      forAll(char) { c => HttpGrammar.parseAll(HttpGrammar.quotedPair, "\\" + c).get should be(c.toString) }
+    }
+
+    it("should parse valid qdtexts") {
+      forAll(qdtext) { text => HttpGrammar.parseAll(HttpGrammar.qdtext, text).get should be(text) }
+    }
+
+    it("should parse valid quoted strings") {
+
     }
   }
 }
