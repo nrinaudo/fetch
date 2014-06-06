265a060407f92a19113a1b93481e1ca84ffa1028
nrinaudo <nicolas@nrinaudo.com>
2014-06-06 22:49:53 +0200

    Status documentation.

--- core/src/main/scala/com/nrinaudo/fetch/Status.scala
+++ core/src/main/scala/com/nrinaudo/fetch/Status.scala
@@ -1,5 +1,6 @@
 package com.nrinaudo.fetch
 
+/** Defines known HTTP status codes, as well as pattern match helpers. */
 object Status {
   val Ok = Status(200)
   val Created = Status(201)
@@ -43,23 +44,84 @@ object Status {
   val GatewayTimeout = Status(504)
   val HttpVersionNotSupported = Status(505)
 
+  /** Helper for status group extractors.
+    *
+    * For example, the following code will create an extractor for the `2xx` group ([[Status.Success]], essentially):
+    * {{{
+    * object Success extends Extractor(_.isSuccess)
+    * }}}
+    *
+    * This can then be used as follows: {{{
+    *   val res: Response[ResponseEntity] = ???
+    *
+    *   // Pattern match against a status
+    *   res.status match {
+    *     case Success(_) => println("Success")
+    *     case _          => println("Not success")
+    *   }
+    *
+    *   // Pattern match against a Response
+    *   res match {
+    *     case Success(_) => println("Success")
+    *     case _          => println("Not success")
+    *   }
+    * }}}
+    *
+    * @param f any status for which this returns `true` will be matched by the extractor.
+    */
   class Extractor(val f: Status => Boolean) {
+    /** Pattern matches against [[Status statuses]]. */
     def unapply(status: Status): Option[Status] =
       if(f(status)) Some(status)
       else          None
 
+    /** Pattern matches against [[Response responses]]. */
     def unapply[T](res: Response[T]): Option[Response[T]] =
       if(f(res.status)) Some(res)
       else              None
   }
 
+  /** Used to pattern match [[Status statuses]] and [[Response responses]] on the `2xx` group. */
   object Success extends Extractor(_.isSuccess)
+  /** Used to pattern match [[Status statuses]] and [[Response responses]] on the `4xx` and `5xx` groups. */
   object Error extends Extractor(_.isError)
+  /** Used to pattern match [[Status statuses]] and [[Response responses]] on the `4xx` group. */
   object ClientError extends Extractor(_.isClientError)
+  /** Used to pattern match [[Status statuses]] and [[Response responses]] on the `5xx` group. */
   object ServerError extends Extractor(_.isServerError)
+  /** Used to pattern match [[Status statuses]] and [[Response responses]] on the `3xx` group. */
   object Redirection extends Extractor(_.isRedirection)
 }
 
+/** Represents an HTTP status code, as defined in
+  * [[http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.1.1 RFC 2616]].
+  *
+  * This class and its companion objects provide various extraction helpers to ease pattern matching.
+  * For example: {{{
+  *   val res: Response[ResponseEntity] = ???
+  *
+  *   res.status match {
+  *     case Status.Success(_) => println("Success")
+  *     case _                 => println("Not success")
+  *   }
+  * }}}
+  *
+  * It's also possible to pattern match directly on instances of [[Response]], which can be useful when
+  * [[Request.map mapping]] on [[Request requests]]: {{{
+  *   val res: Request[Response[ResponseEntity]] = ???
+  *
+  *   res map {
+  *     // Match on the 2xx group
+  *     case Status.Success(res)           => println("Success: " + res.body.as[String])
+  *     // Match on HTTP 500
+  *     case Status.InternalServerError(_) => println("Internal server error")
+  *     // Match on everything else
+  *     case _                             => println("Something else entirely")
+  *   }
+  * }}}
+  *
+  * See the [[Status$ companion object]] for known status codes.
+  */
 case class Status(code: Int) {
   require(code > 0 && code < 600)
 
@@ -80,9 +142,14 @@ case class Status(code: Int) {
 
   override def toString = code.toString
 
+  /** Checks whether the status is a client error (`4xx`). */
   def isClientError = code >= 400 && code < 500
+  /** Checks whether the status is a server error (`5xx`). */
   def isServerError = code >= 500 && code < 600
+  /** Checks whether the status is a redirection (`3xx`). */
   def isRedirection = code >= 300 && code < 400
+  /** Checks whether the status is a client error (`2xx`). */
   def isSuccess     = code >= 200 && code < 300
+  /** Checks whether the status is an error (`4xx` and `5xx`). */
   def isError       = isClientError || isServerError
 }
